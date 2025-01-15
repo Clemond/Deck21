ef71c6a124ed7f201d4a769ecc83149b50231ede
Nicholas <quackshows11@gmail.com>
2025-01-15 23:48:38 +0100

    Feature: Implementet nativewind-css for styling

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View className="bg-green-200 flex-grow justify-center items-center">
       <Text>Hello world!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
