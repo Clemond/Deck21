63667ce3add807e1e13e09d5fbfb312e898fcd75
Nicholas <quackshows11@gmail.com>
2025-01-18 14:42:49 +0100

    Feature: Added interface for cards and types for Suits and values

---
+++ types/ICard.ts
@@ -0,0 +1,20 @@
+export type Suit = "Hearts" | "Diamonds" | "Clubs" | "Spades";
+export type Value =
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "10"
+  | "Jack"
+  | "Queen"
+  | "King"
+  | "Ace";
+
+export interface ICard {
+  suit: Suit;
+  value: Value;
+}
