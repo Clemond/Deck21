2514cb1af3999e23b65464348e2756914914fc16
Nicholas <quackshows11@gmail.com>
2025-03-04 11:11:22 +0100

    feature: implemented a ActivityIndicator when a card is loading

--- components/cards/card.tsx
+++ components/cards/card.tsx
@@ -1,13 +1,24 @@
-import { View, Image } from "react-native";
+import { View, Image, ActivityIndicator } from "react-native";
 import { ICard } from "../../types/ICard";
+import { useState } from "react";
 
 export default function Card({ item }: { item: ICard }) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   return (
     <View className="p-1 mx-2 rounded-lg w-32 shadow-md">
+      {isLoading && (
+        <ActivityIndicator
+          animating={true}
+          color={"#fff"}
+          className="flex-1 justify-end"
+        />
+      )}
       <Image
         source={{ uri: item.image }}
         className="flex-1"
         resizeMode="contain"
+        onLoad={() => setIsLoading(false)}
       />
     </View>
   );
